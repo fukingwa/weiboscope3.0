0021a05c140b0cdbb44dbf3a16ce795bac2be9a3
fukingwa <fukingwa@gmail.com>
2021-05-23 10:32:12 +0800

    dbinserted and deleted_last_seen changed

--- weiboscope_scraper.R
+++ weiboscope_scraper.R
@@ -210,9 +210,9 @@ InsertDB <- function(df){
   dbGetQuery(con, "set client_encoding to 'utf-8'")
   dbinserted_time <- rep(as.character(Sys.time()),nrow(df))
   strSQL <- paste(
-    'insert into rp_sinaweibo (id,user_id,screen_name,retweeted_status_user_id,retweeted_status,created_at,text,original_pic,in_reply_to_screen_name,reposts_count,comments_count,attitudes_count,dbinserted,myip) values',
-    paste(sprintf("(%s,%s,'%s',%s,%s,'%s','%s','%s','%s',%s,%s,%s,'%s','%s')",df$id,tona(df$user_id),gsub("'","''",df$screen_name),df$retweeted_status_user_id,df$retweeted_status,df$created_at,gsub("'","''",df$text),df$original_pic,gsub("'","''",df$in_reply_to_screen_name),tona(df$reposts_count),tona(df$comments_count),tona(df$attitudes_count),dbinserted_time,myip), collapse=', '),
-    'on conflict (id) do update set dbinserted = EXCLUDED.dbinserted, reposts_count = EXCLUDED.reposts_count, comments_count = EXCLUDED.comments_count, attitudes_count = EXCLUDED.attitudes_count', sep = ' '
+    'insert into rp_sinaweibo (id,user_id,screen_name,retweeted_status_user_id,retweeted_status,created_at,text,original_pic,in_reply_to_screen_name,reposts_count,comments_count,attitudes_count,dbinserted,deleted_last_seen,myip) values',
+    paste(sprintf("(%s,%s,'%s',%s,%s,'%s','%s','%s','%s',%s,%s,%s,'%s','%s','%s')",df$id,tona(df$user_id),gsub("'","''",df$screen_name),df$retweeted_status_user_id,df$retweeted_status,df$created_at,gsub("'","''",df$text),df$original_pic,gsub("'","''",df$in_reply_to_screen_name),tona(df$reposts_count),tona(df$comments_count),tona(df$attitudes_count),dbinserted_time,dbinserted_time,myip), collapse=', '),
+    'on conflict (id) do update set deleted_last_seen = EXCLUDED.deleted_last_seen, reposts_count = EXCLUDED.reposts_count, comments_count = EXCLUDED.comments_count, attitudes_count = EXCLUDED.attitudes_count', sep = ' '
   )
   strSQL <- gsub(",NA,",",NULL,",strSQL)
   strSQL <- gsub("\\(NA,","\\(NULL,",strSQL)
@@ -244,7 +244,7 @@ Set_EC <- function(id,ecode){
   con <- dbConnect(dbDriver("PostgreSQL"), user=DB_UNAME, dbname=DB_NAME, host=HOSTIP)
   dbGetQuery(con, "set client_encoding to 'utf-8'")
   current_time <- as.character(Sys.time())
-  strSQL <- paste0("update rp_sinaweibo set (ecode,dbinserted) = (",ifelse(is.na(ecode),"NULL",paste0("'",ecode,"'")),",'",current_time,"') where id = ",id)
+  strSQL <- paste0("update rp_sinaweibo set (ecode,deleted_last_seen) = (",ifelse(is.na(ecode),"NULL",paste0("'",ecode,"'")),",'",current_time,"') where id = ",id)
   dbSendQuery(con, strSQL)
   dbDisconnect(con)
 }
