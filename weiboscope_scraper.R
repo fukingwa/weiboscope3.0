8da47afb07110aaed53cf87be8cae72853ee718b
fukingwa <fukingwa@gmail.com>
2022-04-29 01:46:19 +0800

    ### checking only after 16 hour

--- weiboscope_scraper.R
+++ weiboscope_scraper.R
@@ -736,7 +736,7 @@ while (1) {
   		source("https://raw.githubusercontent.com/fukingwa/weiboscope3.0/main/weiboscope_scraper.R")
 	})
 	
-	need_to_chk <- unique(c(wb_df$user_id,all[all$created_at < (Sys.time() - (checking_time*60*30)),]$user_id))  ### checking only between 12 and 24 hour before 
+	need_to_chk <- unique(c(wb_df$user_id,all[all$created_at < (Sys.time() - (checking_time*60*60*2/3)),]$user_id))  ### checking only after 16 hour 
 	
 	print(paste0("Need to check: ",length(need_to_chk)))
 	
