500a24bae32e983133147796bbdf3b378f00d530
fukingwa <fukingwa@gmail.com>
2022-09-12 18:22:05 +0800

    read -n 1 -s

--- reset.sh
+++ reset.sh
@@ -2,7 +2,7 @@ killall chrome
 killall chromedriver
 killall java
 /home/fukingwa/Weibo/chrome.sh
-read -p "Press enter to continue"
+read -n 1 -s
 /home/fukingwa/Weibo/run_java.sh
-read -p "Press enter to continue"
+read -n 1 -s
 R -e "source('/home/fukingwa/Weibo/run_weiboscope.R')"
